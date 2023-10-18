8a4ab4f0486622a1b306149c10a07826847cefe5
masami-tr <m3o.mmmnnn@gmail.com>
2023-10-18 13:17:08 +0900

    プロジェクトセットアップ

---
+++ src/main/java/com/raisetech98/crudapi231018/Crudapi231018Application.java
@@ -0,0 +1,13 @@
+package com.raisetech98.crudapi231018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudapi231018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudapi231018Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech98/crudapi231018/Crudapi231018ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech98.crudapi231018;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crudapi231018ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
